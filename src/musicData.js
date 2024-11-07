8492dfa48abc1edf98087b5363bdd1bfadfa55bf
Hariprasath V <webhari2005@gmail.com>
2024-11-07 11:56:00 +0530

    edit

--- src/musicData.js
+++ src/musicData.js
@@ -34,6 +34,42 @@ const track = [
       audioSrc: "/music/track5.mp3",
       thumbnail: "/photo/mazhai.jpg" // Add the path to the image thumbnail
     },
+    {
+      id: 6,
+      title: "Oxygen",
+      artist: "Kavan",
+      audioSrc: "/music/track6.mp3",
+      thumbnail: "/photo/oxygen.jpg" // Add the path to the image thumbnail
+    },
+    {
+      id: 7,
+      title: "Naan aval illai",
+      artist: "Mass",
+      audioSrc: "/music/track7.mp3",
+      thumbnail: "/photo/naan.jpg" // Add the path to the image thumbnail
+    },
+    {
+      id: 8,
+      title: "Whistle Podu",
+      artist: "Goat",
+      audioSrc: "/music/track8.mp3",
+      thumbnail: "/photo/whistle.jpg" // Add the path to the image thumbnail
+    },
+    {
+      id: 9,
+      title: "Matta",
+      artist: "Goat",
+      audioSrc: "/music/track9.mp3",
+      thumbnail: "/photo/matta.jpg" // Add the path to the image thumbnail
+    },
+    {
+      id: 9,
+      title: "So-baby",
+      artist: "Doctor",
+      audioSrc: "/music/track10.mp3",
+      thumbnail: "/photo/baby.jpg" // Add the path to the image thumbnail
+    },
+    
   
   ];
   
